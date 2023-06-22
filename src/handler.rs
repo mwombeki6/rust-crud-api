7fa9b73af61208ee419e034fd110f0117aa4e0a9
mwombeki <hackerlubex@protonmail.com>
2023-06-23 00:37:14 +0300

    commit

---
+++ src/handler.rs
@@ -0,0 +1,41 @@
+use crate::{
+    model::NoteModel,
+    schema::{CreateNoteSchema, FilterOptions, UpdateNoteSchema},
+    AppState,
+};
+use actix_web::{delete, get, patch, post, web, HttpResponse, Responder};
+use chrono::prelude::*;
+use serde_json::json;
+
+#[get("/notes")]
+pub async fn note_list_handler(
+    opts: web::Query<FilterOptions>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let limit = opts.limit.unwrap_or(10);
+    let offset = (opts.page.unwrap_or(1) - 1) * limit;
+
+    let query_result = sqlx::query_as!(
+        NoteModel,
+        "SELECT * FROM notes ORDER by id LIMIT $1 OFFSET $2",
+        limit as i32,
+        offset as i32
+    )
+    .fetch_all(&data.db)
+    .await;
+    
+    if query_result.is_err() {
+        let message = "Something went wrong while fetching all note items";
+        return HttpResponse::InternalServerError()
+            .json(json!({"status": "error", "message": message}));
+    }
+
+    let notes = query_result.unwrap();
+
+    let json_response = serde_json::json!({
+        "status": "success",
+        "results": notes.len(),
+        "notes": notes
+    });
+    HttpResponse::Ok().json(json_response)
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use actix_web::middleware::Logger;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde_json::json;
+
+#[get("/api/healthchecker")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "Build Simple CRUD API with Rust, SQLX, Postgres and Actix Web";
+
+    HttpResponse::Ok().json(json!({"status": "success", "message": MESSAGE}))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST-LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::init();
+
+    println!("🚀 Server started successfully");
+
+    HttpServer::new(move || {
+        App::new()
+            .service(health_checker_handler)
+            .wrap(Logger::default())  
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
+}
