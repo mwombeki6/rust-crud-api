c62af8523d1651e106cc443ebbfca47827862880
mwombeki <hackerlubex@protonmail.com>
2023-06-26 23:56:23 +0300

    commit

--- src/handler.rs
+++ src/handler.rs
@@ -75,3 +75,110 @@ async fn create_note_handler(
         }
     }
 }
+
+#[get("/notes/{id}")]
+async fn get_note_handler(
+    path: web::Path<uuid::Uuid>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let note_id = path.into_inner();
+    let query_result = sqlx::query_as!(NoteModel, "SELECT * FROM notes WHERE id = $1", note_id)
+        .fetch_one(&data.db)
+        .await;
+
+    match query_result {
+        Ok(note) => {
+            let note_response = serde_json::json!({"status": "success", "data": serde_json::json!({
+                "note": note
+            })});
+
+            return HttpResponse::Ok().json(note_response);
+        }
+        Err(_) => {
+            let message = format!("Note with ID: {} not found", note_id);
+            return HttpResponse::NotFound()
+                .json(serde_json::json!({"status": "fail", "message": message}));
+        }
+    }
+}
+
+#[patch("/notes/{id}")]
+async fn edit_note_handler(
+    path: web::Path<uuid::Uuid>,
+    body: web::Json<UpdateNoteSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let note_id = path.into_inner();
+    let query_result = sqlx::query_as!(NoteModel, "SELECT * FROM notes WHERE id = $1", note_id)
+        .fetch_one(&data.db)
+        .await;
+
+    if query_result.is_err() {
+        let message = format!("Note with ID: {} not found", note_id);
+        return HttpResponse::NotFound()
+            .json(serde_json::json!({"status": "fail", "message": message}));
+    }
+
+    let now = Utc::now();
+    let note = query_result.unwrap();
+
+    let query_result = sqlx::query_as!(
+        NoteModel,
+        "UPDATE notes SET title = $1, content = $2, category = $3, published = $4, updated_at = $5 WHERE id = $6 RETURNING *",
+        body.title.to_owned().unwrap_or(note.title),
+        body.content.to_owned().unwrap_or(note.content),
+        body.category.to_owned().unwrap_or(note.category.unwrap()),
+        body.published.unwrap_or(note.published.unwrap()),
+        now,
+        note_id
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match query_result {
+        Ok(note) => {
+            let note_response = serde_json::json!({"status": "success", "data": serde_json::json!({
+                "note": note
+            })});
+
+            return HttpResponse::Ok().json(note_response);
+        }
+        Err(err) => {
+            let message = format!("Error: {:?}", err);
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"status": "error", "message": message}));
+        }
+    }
+}
+
+#[delete("/notes/{id}")]
+async fn delete_note_handler(
+    path: web::Path<uuid::Uuid>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let note_id = path.into_inner();
+    let rows_affected = sqlx::query!("DELETE FROM notes WHERE id = $1", note_id)
+        .execute(&data.db)
+        .await
+        .unwrap()
+        .rows_affected();
+
+    if rows_affected == 0 {
+        let message = format!("Note with ID: {} not found", note_id);
+        return HttpResponse::NotFound().json(json!({"status": "fail", "message": message}));
+    }
+
+    HttpResponse::NoContent().finish()
+}
+
+pub fn config(conf: &mut web::ServiceConfig) {
+    let scope = web::scope("/api")
+        .service(health_checker_handler)
+        .service(note_list_handler)
+        .service(create_note_handler)
+        .service(get_note_handler)
+        .service(edit_note_handler)
+        .service(delete_note_handler);
+
+    conf.service(scope);
+}
